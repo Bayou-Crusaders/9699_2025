b326c8a360acaf99d13251585d0ab673e3bcadfc
Bray500000 <braydendurocher5009@gmail.com>
2025-07-03 16:05:38 -0500

    a

--- Swerve_Code/Swerve_Python/robot.py
+++ Swerve_Code/Swerve_Python/robot.py
@@ -26,6 +26,8 @@ class MyRobot(commands2.TimedCommandRobot):
         initialization code.
         """
 
+        TalonFX.setNeutralMode(1)
+
         # Instantiate our RobotContainer.  This will perform all our button bindings, and put our
         # autonomous chooser on the dashboard.
         self.container = RobotContainer()
